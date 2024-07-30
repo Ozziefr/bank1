34aa04f1c9ea831bddaf594007c0e47ff16b3b8a
Ozziefr <yeyerehdjd@gmail.com>
2024-07-30 21:44:34 +0100

    bank

---
+++ account.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const users = {
+    dzxgamer: { password: "1234", balance: 0 },
+    moumouSTOP: { password: "youcef.hdr.", balance: 20000 },
+    shadow: { password: "tizkane", balance: 5000 },
+    liljay: { password: "jadhanine", balance: 0 },
+    kamal: { password: "kamal221199", balance: 0 },
+    player3: { password: "securepassword", balance: 0 },
+  };
+
+  const username = sessionStorage.getItem("username");
+  const messageElement = document.createElement("p");
+
+  if (username && users[username]) {
+    console.log(
+      `Welcome ${username}. Your balance is $${users[username].balance}`
+    );
+    const welcomeMessage = document.getElementById("welcomeMessage");
+    const balanceElement = document.getElementById("balance");
+
+    welcomeMessage.textContent = `Welcome, ${username}!`;
+    balanceElement.textContent = `$${users[username].balance}`;
+  } else {
+    console.log("User not authenticated. Redirecting to index.html");
+    messageElement.textContent =
+      "User not authenticated. Redirecting to home page.";
+    document.body.appendChild(messageElement);
+    setTimeout(() => {
+      window.location.href = "index.html";
+    }, 3000);
+  }
+});
---
+++ index.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("loginForm");
+
+  const users = {
+    dzxgamer: { password: "1234", balance: 0 },
+    moumouSTOP: { password: "youcef.hdr.", balance: 20000 },
+    shadow: { password: "tizkane", balance: 5000 },
+    liljay: { password: "jadhanine", balance: 0 },
+    kamal: { password: "kamal221199", balance: 0 },
+    player3: { password: "securepassword", balance: 0 },
+  };
+
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const messageElement = document.createElement("p");
+    messageElement.style.color = "red";
+
+    if (users[username] && users[username].password === password) {
+      console.log(`Logging in as ${username}. Redirecting to account.html`);
+      sessionStorage.setItem("username", username);
+      window.location.href = "account.html";
+    } else {
+      console.log("Incorrect username or password");
+      messageElement.textContent = "Incorrect username or password";
+      loginForm.appendChild(messageElement);
+    }
+  });
+});
